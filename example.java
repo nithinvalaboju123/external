867b83f4e2431e15902b72d3f935afaa471d08ba
nithinvalaboju123 <227r1a05r1@cmrtc.ac.in>
2024-12-11 14:44:45 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,9 @@
+public class Example{
+	public static void main(String args[]){
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println("HELLO WORLD");
+		System.out.println("RESULT IS:"+c);
+	}
+}
